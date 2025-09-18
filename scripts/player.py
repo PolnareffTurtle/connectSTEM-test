ae927618aee895b500a83d55f1576248eb99b4b4
jiaenwang <jiaenpeacewang@gmail.com>
2025-09-17 19:19:16 -0700

    Preventing player from going out of bounds

--- scripts/player.py
+++ scripts/player.py
@@ -5,6 +5,8 @@ class Player(Entity):
     def __init__(self,game,pos):
         super().__init__(game, pos, 'player')
         self.speed = 5
+        self.max_health = 100
+        self.health = 100
 
     def update(self,movement):
         self.rect.x += movement[0] * self.speed
@@ -12,11 +14,9 @@ class Player(Entity):
         if self.rect.left < 0:
             self.rect.left = 0
         # use the internal display surface for bounds checks (rendering is to `game.display`)
-        display_w = self.game.display.get_width()
-        display_h = self.game.display.get_height()
-        if self.rect.right > display_w:
-            self.rect.right = display_w
+        if self.rect.right > self.game.display.get_width():
+            self.rect.right = self.game.display.get_width()
         if self.rect.top < 0:
             self.rect.top = 0
-        if self.rect.bottom > display_h:
-            self.rect.bottom = display_h
+        if self.rect.bottom > self.game.display.get_height():
+            self.rect.bottom = self.game.display.get_height()
