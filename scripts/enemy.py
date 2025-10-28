c44dff0fc8de22939e6f802a640c5d80e3c74702
sephliu1029-blip <seph.liu1029@gmail.com>
2025-10-27 22:57:21 -0500

    you can change the health but it's just for simplicity rn, the "die()" ensures there are no same drops, appends the new coinc class to self.game.coins (shared list), and randomizes the drop location  "render()" draws enemy

--- scripts/enemy.py
+++ scripts/enemy.py
@@ -1,6 +1,25 @@
 import pygame
 from scripts.entities import Entity
+from scripts.economy import Coin
+from random import randint
 
 class Enemy(Entity):
     def __init__(self,game,pos):
         super().__init__(game, pos, 'enemy')
+        self.health = 1  #change if want
+        self.alive = True
+
+    def die(self):
+        #drop coin when enemy die
+        if not self.alive:
+            return
+        self.alive = False
+        drop_pos = (self.pos[0] + randint(-3, 3), self.pos[1] + randint(-3, 3))
+        coin = Coin(self.game, drop_pos)
+        if not hasattr(self.game, 'coins'):
+            self.game.coins = []
+        self.game.coins.append(coin)
+
+    def render(self, surf):
+        if self.alive:
+            surf.blit(self.game.assets['enemy'], self.pos)
