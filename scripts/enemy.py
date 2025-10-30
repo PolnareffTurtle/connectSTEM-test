a319ce0d067e0f8c2175c43903006551bb965463
sephliu1029-blip <seph.liu1029@gmail.com>
2025-10-30 16:02:36 -0500

    random enemy spawns, scaled difficulty per wave, enemy and attack slightly randomized. modified the init for this purpose too.

--- scripts/enemy.py
+++ scripts/enemy.py
@@ -4,9 +4,10 @@ from scripts.economy import Coin
 from random import randint
 
 class Enemy(Entity):
-    def __init__(self,game,pos):
+    def __init__(self, game, pos, health = 6, attack = 1):
         super().__init__(game, pos, 'enemy')
-        self.health = 1  #change if want
+        self.health = health #scales w/ wave # later
+        self.attack = attack #can scale for harder enemies
         self.alive = True
 
     def die(self):
@@ -16,6 +17,21 @@ class Enemy(Entity):
         self.alive = False
         drop_pos = (self.pos[0] + randint(-3, 3), self.pos[1] + randint(-3, 3))
         coin = Coin(self.game, drop_pos)
+        #ensure game can append to coin list
         if not hasattr(self.game, 'coins'):
             self.game.coins = []
+
         self.game.coins.append(coin)
+
+    def create_wave(game, wave_number, count):
+        enemies = [] #to create and return list of enemies per wave
+        for i in range(count):
+            #scales attack & health w/ wave
+            health = 5 + wave_number * 2
+            attack = 1 + wave_number//2
+            #random spawn positions
+            pos = (randint(50, game.display.get_width() - 50),
+                   randint(50, game.display.get_height() -50))
+
+            enemies.append(Enemy(game, pos, health, attack))
+        return enemies
