00671776d0a094bda227ce91a51fbd199d850f0b
TaehongK <taehongkim001@gmail.com>
2026-03-21 23:38:09 -0400

    Added gun to weapon manager,procedural tilemap, moved gun from player to weapon manager

--- scripts/weaponmanager.py
+++ scripts/weaponmanager.py
@@ -1,43 +1,45 @@
 import pygame
-from scripts.weapon import CircleWeapon, RotateWeapon
+from scripts.weapon import CircleWeapon, RotateWeapon, Gun
 from scripts.enums import WeaponType
 
 class WeaponManager:
-    def __init__(self,player):
+    def __init__(self, player):
         self.player = player
 
-        self.weapons=[
-            CircleWeapon(attack_power=10,attack_speed=1/1.5, attack_radius=50),
-            RotateWeapon(attack_power=5,attack_speed=4,radius=40,rotation_speed=480),
+        self.weapons = [
+            CircleWeapon(attack_power=10, attack_speed=1/1.5, attack_radius=50),
+            RotateWeapon(attack_power=5, attack_speed=4, radius=40, rotation_speed=480),
+            Gun(attack_speed=5, attack_power=1, bullet_speed=600),
         ]
-        
+
         self.active_index = 0
         self.active_weapon = self.weapons[self.active_index]
-    
 
     def switch_weapon(self):
-        self.active_index = (self.active_index+1)%len(self.weapons)
+        self.active_index = (self.active_index + 1) % len(self.weapons)
         self.active_weapon = self.weapons[self.active_index]
-    
+
     def use_weapon(self, targets):
-        self.active_weapon.use(self.player,targets=targets)
-    
+        self.active_weapon.use(self.player, targets=targets)
+
     def needs_continuous_attack(self):
-        return self.active_weapon.type ==WeaponType.ROTATE
-    
+        return self.active_weapon.type == WeaponType.ROTATE
+
     def update(self, dt):
         for weapon in self.weapons:
             weapon.update(dt)
 
     def handle_input(self, keys, targets):
-        if keys[pygame.K_SPACE]:
+        if isinstance(self.active_weapon, Gun):
+            self.active_weapon.handle_input(self.player, targets=targets)
+        elif keys[pygame.K_SPACE]:
             self.use_weapon(targets)
-    
-    def handle_event(self,event):
+
+    def handle_event(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 self.switch_weapon()
 
-    def render_weapon_visual(self,screen,offset=(0, 0)):
-        if hasattr(self.active_weapon,'render'):
-            self.active_weapon.render(screen,self.player,offset)
+    def render_weapon_visual(self, screen, offset=(0, 0)):
+        if hasattr(self.active_weapon, 'render'):
+            self.active_weapon.render(screen, self.player, offset)
